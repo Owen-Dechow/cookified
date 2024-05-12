81b2ec62660bce7cc7213179a48264fb856cbc21
Owen-Dechow <owen,dechow@gmail.com>
2024-05-11 21:23:19 -0400

    Add base dir to static root

--- cookified/settings.py
+++ cookified/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = "staticfilecollection/"
+STATIC_ROOT = BASE_DIR / "staticfilecollection/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
